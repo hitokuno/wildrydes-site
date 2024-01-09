db9ae70c8a8eda52e385e59174d17293a761d18a
Hirokazu Tokuno <hitokuno@gmail.com>
2024-01-09 23:52:50 +0900

    update config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_MIlz9fkrW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7p4blsi7m3r5n21k5if16pdbsc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
